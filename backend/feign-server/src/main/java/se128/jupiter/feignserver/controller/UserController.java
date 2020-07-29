b359aa340a0e3514a94f2d8f01e6d3e1f38899e0
HaoningLan <546004464@qq.com>
2020-07-29 13:17:29 +0800

    add common

--- backend/feign-server/src/main/java/se128/jupiter/feignserver/controller/UserController.java
+++ backend/feign-server/src/main/java/se128/jupiter/feignserver/controller/UserController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se128.jupiter.feignserver.service.UserService;
+import util.msgutils.Msg;
 
 @RestController
 @RequestMapping("/user")
