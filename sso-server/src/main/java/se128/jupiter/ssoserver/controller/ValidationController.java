46733bfc7ec577f84f0df4e65562d5549a697680
HeyCat-svg <1345226109@qq.com>
2020-08-23 09:40:32 +0800

    update: sso-server

--- sso-server/src/main/java/se128/jupiter/ssoserver/controller/ValidationController.java
+++ sso-server/src/main/java/se128/jupiter/ssoserver/controller/ValidationController.java
@@ -1,13 +1,17 @@
 package se128.jupiter.ssoserver.controller;
 
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOError;
+import java.io.IOException;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 @RestController
@@ -33,16 +37,39 @@ public class ValidationController {
     public String checkUsernameAndPassword(@PathVariable("username") String username, @PathVariable("password") String password) {
         String flag = null;
         if ("huanzi".equals(username) && "123456".equals(password)) {
-            //用户名+时间戳（这里只是demo，正常项目的令牌应该要更为复杂）
+            // 用户名+时间戳（这里只是demo，正常项目的令牌应该要更为复杂）
             flag = username + System.currentTimeMillis();
-            //令牌作为key，存用户id作为value（或者直接存储可暴露的部分用户信息也行）设置过期时间（我这里设置3分钟）
+            // 令牌作为key，存用户id作为value（或者直接存储可暴露的部分用户信息也行）设置过期时间（我这里设置3分钟）
             template.opsForValue().set(flag, "1", (long) (3 * 60), TimeUnit.SECONDS);
         }
         return flag;
     }
 
+    @PostMapping("/login")
+    public String login(HttpServletResponse response, @RequestBody Map<String, String> params){
+        String username = params.get("username");
+        String password = params.get("password");
+
+        String check = checkUsernameAndPassword(username, password);
+        if(!StringUtils.isEmpty(check)){
+            try{
+                Cookie cookie = new Cookie("accessToken", check);
+                cookie.setMaxAge(60 * 3);
+                // 设置域
+                // cookie.setDomain("localhost:8801");
+                cookie.setPath("/");
+                response.addCookie(cookie);
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+            return "login success";
+        }
+        return "login failed";
+    }
+
     @RequestMapping("/checkSession")
     public boolean checkSession(){
+
         return true;
     }
 
--- zuul-server/src/main/java/se128/jupiter/zuulserver/filter/AccessFilter.java
+++ zuul-server/src/main/java/se128/jupiter/zuulserver/filter/AccessFilter.java
@@ -3,11 +3,21 @@ package se128.jupiter.zuulserver.filter;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 import com.netflix.zuul.exception.ZuulException;
+import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import se128.jupiter.zuulserver.service.SsoFeign;
+import se128.jupiter.zuulserver.util.msgutils.Msg;
+import se128.jupiter.zuulserver.util.msgutils.MsgCode;
+import se128.jupiter.zuulserver.util.msgutils.MsgUtil;
+import sun.swing.StringUIClientPropertyKey;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOError;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class AccessFilter extends ZuulFilter {
     public AccessFilter(){}
@@ -36,10 +46,42 @@ public class AccessFilter extends ZuulFilter {
         HttpServletRequest request = ctx.getRequest();
         HttpServletResponse response = ctx.getResponse();
 
-        ssoFeign.checkUsernameAndPassword("huanzi", "123456");
+        // 访问路径
+        StringBuilder url = new StringBuilder(request.getRequestURL().toString());
 
-        ctx.setSendZuulResponse(true);
-        ctx.setResponseStatusCode(200);
+        // 获取cookies
+        String accessToken  = "";
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if("accessToken".equals(cookie.getName())){
+                    accessToken = cookie.getValue();
+                }
+            }
+        }
+
+        // 过滤规则
+        if (url.toString().contains("sso/login") ||
+                url.toString().contains("sso/checkSession") ||
+                (!StringUtils.isEmpty(accessToken) && ssoFeign.hasKey(accessToken))){
+            ctx.setSendZuulResponse(true);
+            ctx.setResponseStatusCode(200);
+
+            System.out.println("INNNNNNNNNNNNN");
+        }
+        else{
+            // send json back
+            Msg msg = MsgUtil.makeMsg(MsgCode.NOT_AVAILABLE);
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json; charset=utf-8");
+            try (PrintWriter writer = response.getWriter()){
+                writer.print(JSONObject.fromObject(msg));
+            }catch (IOException e){
+                System.out.println("send json back error");
+            }
+
+            System.out.println("OUTTTTTTTTTTTTTT");
+        }
         return null;
     }
 }
